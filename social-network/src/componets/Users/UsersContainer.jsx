ed8ac193969bef6bd412bd02058bd28f662bcc8c
Grenvals <grenvals@gmail.com>
2020-04-16 17:32:50 +0300

    refactored users container component

--- social-network/src/componets/Users/UsersContainer.jsx
+++ social-network/src/componets/Users/UsersContainer.jsx
@@ -1,7 +1,38 @@
-// import React from 'react'
+import React from 'react'
+import Users from './Users'
+import Preloader from '../common/Preloader/Preloader'
 import { connect } from 'react-redux'
-import  UsersContainerAPI from './UsersContainerAPI'
 import {followUser, unfollowUser, setCurrentPage, getUsers} from '../../redux/usersReducer'
+
+class UsersContainerAPI extends React.Component {
+  componentDidMount() {
+    this.props.getUsers(this.props.currentPage, this.props.pageSize)
+  }
+  onPageChanget = pageNumber => {
+    this.props.getUsers(pageNumber, this.props.pageSize)
+    this.props.setCurrentPage(pageNumber)
+  }
+  render() {
+    return (
+      <>
+        {this.props.isLoading ? (
+          <Preloader />
+        ) : (
+          <Users
+            users={this.props.users}
+            totalUsersCount={this.props.totalUsersCount}
+            pageSize={this.props.pageSize}
+            currentPage={this.props.currentPage}
+            onPageChanget={this.onPageChanget}
+            followingInProgress={this.props.followingInProgress}
+            unfollowUser={this.props.unfollowUser}
+            followUser={this.props.followUser}
+          />
+        )}
+      </>
+    )
+  }
+}
 
 let mapStateToProps = state => {
   return {
