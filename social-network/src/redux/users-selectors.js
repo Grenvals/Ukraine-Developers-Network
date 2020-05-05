25b35cde0ce58101fe85200dcbebeacb37ad937f
Grenvals <grenvals@gmail.com>
2020-05-05 20:17:44 +0300

    add reselect, connect to getUsers selector

--- social-network/src/redux/users-selectors.js
+++ social-network/src/redux/users-selectors.js
@@ -1,18 +1,29 @@
-export const getUsers = state => {
+import { createSelector } from 'reselect'
+
+const getUsersSelector = state => {
   return state.usersPage.users
 }
+
+export const getUsers = createSelector(getUsersSelector, users => {
+  return users.filter(u => true)
+})
+
 export const getPageSize = state => {
   return state.usersPage.pageSize
 }
+
 export const getTotalUsersCount = state => {
   return state.usersPage.totalUsersCount
 }
+
 export const getCurrentPage = state => {
   return state.usersPage.currentPage
 }
+
 export const getIsLoading = state => {
   return state.usersPage.isLoading
 }
+
 export const getFollowingInProgress = state => {
   return state.usersPage.followingInProgress
 }
