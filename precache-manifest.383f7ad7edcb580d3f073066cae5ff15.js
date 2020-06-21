c4af1e8432f552338c487a205b5cf85cecec685c
Grenvals <grenvals@gmail.com>
2020-06-21 16:32:06 +0300

    Updates

---
+++ precache-manifest.383f7ad7edcb580d3f073066cae5ff15.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68855e6462b2148934689d9acbd1022c",
+    "url": "/Ukraine-Develorers-Network/index.html"
+  },
+  {
+    "revision": "192778b5000adc095c7d",
+    "url": "/Ukraine-Develorers-Network/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "23bc7cf082d922e322eb",
+    "url": "/Ukraine-Develorers-Network/static/css/3.e6baf954.chunk.css"
+  },
+  {
+    "revision": "959bff69e75a1b7593b9",
+    "url": "/Ukraine-Develorers-Network/static/css/main.d7ac705f.chunk.css"
+  },
+  {
+    "revision": "192778b5000adc095c7d",
+    "url": "/Ukraine-Develorers-Network/static/js/2.beff8ae4.chunk.js"
+  },
+  {
+    "revision": "6d81ac96324530497b35732420451455",
+    "url": "/Ukraine-Develorers-Network/static/js/2.beff8ae4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23bc7cf082d922e322eb",
+    "url": "/Ukraine-Develorers-Network/static/js/3.353c3827.chunk.js"
+  },
+  {
+    "revision": "959bff69e75a1b7593b9",
+    "url": "/Ukraine-Develorers-Network/static/js/main.de91bb06.chunk.js"
+  },
+  {
+    "revision": "0422777fa9d1ecdba365",
+    "url": "/Ukraine-Develorers-Network/static/js/runtime-main.e4e1f460.js"
+  },
+  {
+    "revision": "5487bea91cef82008fdc6e874b4c5c86",
+    "url": "/Ukraine-Develorers-Network/static/media/OpenSans-Bold.5487bea9.eot"
+  },
+  {
+    "revision": "759ea19bccf8a5ca50a58e474aced157",
+    "url": "/Ukraine-Develorers-Network/static/media/OpenSans-Bold.759ea19b.woff"
+  },
+  {
+    "revision": "93fe8f3415688d16bb947e2c96ea380e",
+    "url": "/Ukraine-Develorers-Network/static/media/OpenSans-Bold.93fe8f34.woff2"
+  },
+  {
+    "revision": "559e978f49dc58dbe4213c714d308483",
+    "url": "/Ukraine-Develorers-Network/static/media/OpenSans-Light.559e978f.woff2"
+  },
+  {
+    "revision": "c002a58fc48427af4884fcce2d5c03ee",
+    "url": "/Ukraine-Develorers-Network/static/media/OpenSans-Light.c002a58f.woff"
+  },
+  {
+    "revision": "d8ba00bc265c4a0554703cf89fc35e6e",
+    "url": "/Ukraine-Develorers-Network/static/media/OpenSans-Light.d8ba00bc.eot"
+  },
+  {
+    "revision": "38ca9e2302c8793c93f854e8595eed78",
+    "url": "/Ukraine-Develorers-Network/static/media/OpenSans-Regular.38ca9e23.eot"
+  },
+  {
+    "revision": "5d5735e57127db2f7a2ad879fc6056b8",
+    "url": "/Ukraine-Develorers-Network/static/media/OpenSans-Regular.5d5735e5.woff2"
+  },
+  {
+    "revision": "6fde2eb6728eb97fd914dcd0641d332b",
+    "url": "/Ukraine-Develorers-Network/static/media/OpenSans-Regular.6fde2eb6.woff"
+  },
+  {
+    "revision": "7106bb83a0e767e12659de9cfba3926d",
+    "url": "/Ukraine-Develorers-Network/static/media/OpenSans-SemiBold.7106bb83.woff2"
+  },
+  {
+    "revision": "e0d5280b186197bd2a77b9babfe5506d",
+    "url": "/Ukraine-Develorers-Network/static/media/OpenSans-SemiBold.e0d5280b.eot"
+  },
+  {
+    "revision": "fe241c6c4f5a190e821b7c03e337d756",
+    "url": "/Ukraine-Develorers-Network/static/media/OpenSans-SemiBold.fe241c6c.woff"
+  },
+  {
+    "revision": "36e2c632087ddbd7026f0a45bcea1f3a",
+    "url": "/Ukraine-Develorers-Network/static/media/P1013597.36e2c632.JPG"
+  },
+  {
+    "revision": "d91f61086ad3405f7b99aa12908a2ea6",
+    "url": "/Ukraine-Develorers-Network/static/media/P10рр132.d91f6108.JPG"
+  },
+  {
+    "revision": "581887835f18e8eb3a8fd1a4b4af65e8",
+    "url": "/Ukraine-Develorers-Network/static/media/The_Xx_-_The_Xx_Intro(uzimusic.ru).58188783.mp3"
+  },
+  {
+    "revision": "095ab7d3fa387500de11a82116cbb9b1",
+    "url": "/Ukraine-Develorers-Network/static/media/background.095ab7d3.jpg"
+  },
+  {
+    "revision": "4024237ef5c0b4181813316541fc089c",
+    "url": "/Ukraine-Develorers-Network/static/media/check.4024237e.svg"
+  },
+  {
+    "revision": "47f0a457df227610fc2f2302b50fad2f",
+    "url": "/Ukraine-Develorers-Network/static/media/dialoglist.47f0a457.svg"
+  },
+  {
+    "revision": "89adbe24a86a9e7ca60fc6e4964ae6e6",
+    "url": "/Ukraine-Develorers-Network/static/media/edit-white.89adbe24.svg"
+  },
+  {
+    "revision": "6e1f468d770f424809efc6ff81ff28cb",
+    "url": "/Ukraine-Develorers-Network/static/media/edit.6e1f468d.svg"
+  },
+  {
+    "revision": "da4cf719c23cdcc4766245f03ef4de34",
+    "url": "/Ukraine-Develorers-Network/static/media/error.da4cf719.svg"
+  },
+  {
+    "revision": "c509a4c46008514b5a5084d2596c99e4",
+    "url": "/Ukraine-Develorers-Network/static/media/facebook.c509a4c4.svg"
+  },
+  {
+    "revision": "dc464dd99207175ac5bf58f6cdc07324",
+    "url": "/Ukraine-Develorers-Network/static/media/file.dc464dd9.svg"
+  },
+  {
+    "revision": "bf3bcf8ccd01d1f501c97acb27445410",
+    "url": "/Ukraine-Develorers-Network/static/media/follow.bf3bcf8c.svg"
+  },
+  {
+    "revision": "a112a001d5170d13d5f46d9387e526a4",
+    "url": "/Ukraine-Develorers-Network/static/media/github.a112a001.svg"
+  },
+  {
+    "revision": "d19e335b06f18ac00590621df2825b81",
+    "url": "/Ukraine-Develorers-Network/static/media/heart.d19e335b.svg"
+  },
+  {
+    "revision": "f8af51e684ba0cf0b2d90ef23909c4a4",
+    "url": "/Ukraine-Develorers-Network/static/media/instagram.f8af51e6.svg"
+  },
+  {
+    "revision": "4985dab9096ff2962ddb15ffb0976dff",
+    "url": "/Ukraine-Develorers-Network/static/media/jobs-1.4985dab9.jpg"
+  },
+  {
+    "revision": "18ccf81b75ebea194b9c4faefe949627",
+    "url": "/Ukraine-Develorers-Network/static/media/left-arrow.18ccf81b.svg"
+  },
+  {
+    "revision": "cbabf14acd95f7e8ed77f3029e819f3b",
+    "url": "/Ukraine-Develorers-Network/static/media/message-white.cbabf14a.svg"
+  },
+  {
+    "revision": "fd72db54474f3c1a8e5b0fd7a022d90c",
+    "url": "/Ukraine-Develorers-Network/static/media/message.fd72db54.svg"
+  },
+  {
+    "revision": "f8c8b133873dd90f200095a84873fce3",
+    "url": "/Ukraine-Develorers-Network/static/media/newspaper.f8c8b133.svg"
+  },
+  {
+    "revision": "b0c738729ab7ecf951af2a6711e0436e",
+    "url": "/Ukraine-Develorers-Network/static/media/notification.b0c73872.svg"
+  },
+  {
+    "revision": "6ec77c6e6adead72c40b13b8e0553c7a",
+    "url": "/Ukraine-Develorers-Network/static/media/pause.6ec77c6e.svg"
+  },
+  {
+    "revision": "8c01ad4a148076bfba78122443aa3345",
+    "url": "/Ukraine-Develorers-Network/static/media/play.8c01ad4a.svg"
+  },
+  {
+    "revision": "135ded44f8cb1f315bfe497602afe56f",
+    "url": "/Ukraine-Develorers-Network/static/media/profile-bg.135ded44.jpg"
+  },
+  {
+    "revision": "d81d0d74f5eb109bd9e2e1bc6bd58abc",
+    "url": "/Ukraine-Develorers-Network/static/media/reload.d81d0d74.svg"
+  },
+  {
+    "revision": "9279a12a9334d7682b605b2d12f181f9",
+    "url": "/Ukraine-Develorers-Network/static/media/right-arrow.9279a12a.svg"
+  },
+  {
+    "revision": "e145b70754e6cb734b07954a2eab942f",
+    "url": "/Ukraine-Develorers-Network/static/media/settings-bg.e145b707.jpg"
+  },
+  {
+    "revision": "633c763540b5da951c552de8dfb10ba5",
+    "url": "/Ukraine-Develorers-Network/static/media/settings.633c7635.svg"
+  },
+  {
+    "revision": "0461c630e2a1f2bc880064e894050cbb",
+    "url": "/Ukraine-Develorers-Network/static/media/site.0461c630.svg"
+  },
+  {
+    "revision": "a57ea219084451288a1f02aabba16f1b",
+    "url": "/Ukraine-Develorers-Network/static/media/twitter.a57ea219.svg"
+  },
+  {
+    "revision": "e51ba20ee6a6dbcea870967cc2bed21b",
+    "url": "/Ukraine-Develorers-Network/static/media/unfollow.e51ba20e.svg"
+  },
+  {
+    "revision": "7a21cbe90fd31540fb0384792fbc4a86",
+    "url": "/Ukraine-Develorers-Network/static/media/user-default.7a21cbe9.svg"
+  },
+  {
+    "revision": "c90b1dceae8477617b65e9d19edef831",
+    "url": "/Ukraine-Develorers-Network/static/media/user-icon.c90b1dce.svg"
+  },
+  {
+    "revision": "642a2607f7eba5b705ffdbf9ee8f8c8a",
+    "url": "/Ukraine-Develorers-Network/static/media/user.642a2607.svg"
+  },
+  {
+    "revision": "2ddadcfb9aae548a58cd3491a4a89749",
+    "url": "/Ukraine-Develorers-Network/static/media/users.2ddadcfb.svg"
+  }
+]);
